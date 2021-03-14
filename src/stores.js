79b73276f040ea28bdfdf2876d2cddf69f757869
Abby <i@abby.md>
2021-03-15 05:39:40 +0900

    go

--- src/stores.js
+++ src/stores.js
@@ -5,3 +5,5 @@ export const userInfoStore = writable(null);
 export const avatarStore = writable({});
 export const usernameStore = writable({});
 export const displaynameStore = writable({});
+
+export const docClicked = writable(false);
