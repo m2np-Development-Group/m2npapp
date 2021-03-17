a889a4bc09766dc53d47c2ecee16734f35f62047
abby <abby@kabu.com>
2021-03-17 19:34:16 +0900

    carousel

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,35 @@
+<script>
+	import {fade} from 'svelte/transition'
+	
+	const carouselPhotos = [
+		'https://picsum.photos/300/200?random=1',
+		'https://picsum.photos/300/200?random=2',
+		'https://picsum.photos/300/200?random=3'
+	]
+	
+	let index = 0
+	
+	const next = () => {
+		index++
+	}
+	const prev = () => {
+		index--
+	}
+</script>
+
+<style>
+	img, button {
+		position: absolute;
+	}
+</style>
+
+{#each [carouselPhotos[index]] as src (index)}
+	<img transition:fade {src} alt="" />	
+{/each}
+
+{#if index < carouselPhotos.length - 1 }
+<button style='right:0' on:click={next}>Next!</button>
+{/if}
+{#if index > 0}
+<button style='left:0' on:click={prev}>Prev!</button>
+{/if}
